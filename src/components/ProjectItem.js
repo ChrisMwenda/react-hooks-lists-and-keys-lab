f3669814277fa40a117c87afe5d197b0491047f4
Christopher <chrismbogo97@gmail.com>
2022-10-01 09:31:33 +0300

    Completed Assignment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,6 +6,8 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
+      {technologies.map(method=>{
+      return<span key={method}>{method}</span>})}
         {/* render a <span> for each technology in the technologies array */}
       </div>
     </div>
