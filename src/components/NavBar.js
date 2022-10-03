d8919fd6d8ad1557ac739ad6f4441c02dcc48ac3
Christopher <chrismbogo97@gmail.com>
2022-10-03 21:32:02 +0300

    DOne

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,16 +9,5 @@ function NavBar() {
     })}
   </nav>;
 }
-import React from "react";
-
-function NavBar() {
-  const links = ["home", "about", "projects"];
-
-  return <nav>
-        {links.map(link => {
-      return <a key={link} href={"#"+link}>{link}</a>
-    })}
-  </nav>;
-}
 
 export default NavBar;
